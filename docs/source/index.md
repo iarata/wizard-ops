dea5b2e259be754b4e76a6b09b74cd282d3fe8cb
Alireza Hajebrahimi <6937697+iarata@users.noreply.github.com>
2026-01-09 09:48:44 +0100

    first commit

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for wizard_ops
---
+++ src/wizard_ops/train.py
@@ -0,0 +1,10 @@
+from wizard_ops.model import Model
+from wizard_ops.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
