e09c01f872b4489f206b9583142d0462f250cd6f
Alireza Hajebrahimi <6937697+iarata@users.noreply.github.com>
2026-01-17 19:29:20 +0100

    Update tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,9 +3,7 @@ import lightning as L
 import pandas as pd
 import pytest
 from torch.utils.data import Dataset
-from wizard_ops.data-cli import Nutrition
-from wizard_ops.data-cli import NutritionDataset
-from wizard_ops.data-cli import get_default_transforms, load_dish_metadata, load_ingredients_metadata
+from wizard_ops.data import Nutrition, get_default_transforms
 
 
 def test_nutrition_dataset_init(tmp_path):
